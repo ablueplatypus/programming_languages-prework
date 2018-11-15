d456ee0c3244b07641ea29c9b94ebdb52c91e601
Matt Doering <mattdoering86@gmail.com>
2018-11-15 19:10:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,6 +4,7 @@ def reformat_languages(languages)
   languages.each do |style, lang_obj|
     lang_obj.each do |language, type|
     new_hash[language] = type
+    new_hash[:style] = [style]
     end
   end
   new_hash
