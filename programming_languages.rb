9793871136fa9ee1a964ca5a1f67de0beda2c329
Matt Doering <mattdoering86@gmail.com>
2018-11-15 19:01:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,9 @@
+require 'pry'
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, lang_obj|
     lang_obj.each do |language, type|
-
+    binding.pry
+    end
+  end
 end
